c669b31fbadbd3cb62f9b5d721718fbaa646f1a2
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-03-06 20:07:20 -0500

    try indexhtml

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,56 @@
-// getting data from the json file
-d3.json("Resources/samples.json").then((data)=> {
-    console.log(data);
-}
+// Creating function for Data plotting (Bar, gauge, bubble)
+//function getPlot(id) {
 
-// create data variable
-var data = [trace];
-  
-// create layout variable to set plots layout
-var layout = {
-    title: ""
+    // getting data from the json file
+    d3.json("./Resources/samples.json").then((data)=> {
+        console.log(data);
+   
+    
+        ///////////////// bar ////////////////
+        var sample = data.samples.filter(item => item.sample.toString() === id)[0];
+        console.log(sample);
+        //sample_values
 
-};
+        //otu_ids
+        
+        // otu_labels
+        
 
-// create the bar plot
-Plotly.newPlot("type", data, layout);
+        // // create data variable
+        // var data1 = [trace];
+        
+        // // create layout variable to set plots layout
+        // var layout = {
+        //     title: ""
+
+        // };
+
+        // // create the bar plot
+        // Plotly.newPlot("bar", data1, layout1);
+
+        /////////// bubble ////////////
+
+        /* Use otu_ids for the x values.
+        Use sample_values for the y values.
+        Use sample_values for the marker size.
+        Use otu_ids for the marker colors.
+        Use otu_labels for the text values. */
+
+        // // create the bar plot
+        //Plotly.newPlot("bubble", data2, layout2);
+
+
+        ////////// gague //////////////
+
+        // // create the bar plot
+        // Plotly.newPlot("gauge", data3, layout3);
+    
+    });
+//}
+
+// function init() {
+//     getPlot(id);
+// }
+// init();
+
+//getPlot(id);
